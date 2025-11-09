a74d5523fdd04c7fde646b731be194700557828c
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-09 23:52:17 +0300

    Add MySQL database connection functions

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python3
+import mysql.connector
+import csv
+import uuid
 
+def connect_db():
+    """Connect to MySQL server"""
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='root'
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
+
+
+def create_database(connection):
+    """Create ALX_prodev database"""
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev;")
+    connection.commit()
+    cursor.close()
+
+
+def connect_to_prodev():
+    """Connect to ALX_prodev DB"""
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            database='ALX_prodev'
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
