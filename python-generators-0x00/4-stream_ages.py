1f2c55d5b97262eeed0ba25edf9348c812dbdae5
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-09 23:59:04 +0300

    Add functions to stream and compute average user age

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+import seed
+
+def stream_user_ages():
+    """Yield ages one by one"""
+    conn = seed.connect_to_prodev()
+    cursor = conn.cursor()
+    cursor.execute("SELECT age FROM user_data")
+
+    for (age,) in cursor:
+        yield int(age)
+
+    cursor.close()
+    conn.close()
+
+
+def compute_average_age():
+    total = 0
+    count = 0
+
+    for age in stream_user_ages():
+        total += age
+        count += 1
+
+    avg = total / count if count else 0
+    print(f"Average age of users: {avg}")
 
