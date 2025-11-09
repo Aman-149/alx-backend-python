d7b4e3429c290d233d51cfeef6fac04c5a72d6e5
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-09 23:57:28 +0300

    Refactor user streaming to yield one user at a time

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
+"""0-stream_users.py
+Generator that streams rows from the user_data table
+"""
+
 import seed
 
-def stream_users_in_batches(batch_size):
-    """Generator that yields rows in batches"""
-    conn = seed.connect_to_prodev()
-    cursor = conn.cursor(dictionary=True)
+
+def stream_users():
+    """Yields one user at a time from the user_data table"""
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM user_data")
 
-    batch = []
     for row in cursor:
-        batch.append(row)
-        if len(batch) == batch_size:
-            yield batch
-            batch = []
-
-    if batch:
-        yield batch
+        yield row
 
     cursor.close()
-    conn.close()
-def batch_processing(batch_size):
-    """Process users > 25 years using the above generator"""
-    for batch in stream_users_in_batches(batch_size):
-        for user in batch:
-            if int(user['age']) > 25:
-                print(user)
+    connection.close()
