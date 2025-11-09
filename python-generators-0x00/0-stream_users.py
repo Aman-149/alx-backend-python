08d0a695a0c7264eb4b85896e2b8a04bcca78f65
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-09 23:54:49 +0300

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+import seed
 
+def stream_users_in_batches(batch_size):
+    """Generator that yields rows in batches"""
+    conn = seed.connect_to_prodev()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    batch = []
+    for row in cursor:
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+
+    if batch:
+        yield batch
+
+    cursor.close()
+    conn.close()
