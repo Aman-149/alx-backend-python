9d687ab1c149dc2e2564caa8de284cefec631460
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-12 22:28:14 +0300

    Add ExecuteQuery class for database operations
    Implement context manager for executing SQL queries.

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,1 +1,16 @@
+import sqlite3
 
+class ExecuteQuery:
+    def __init__(self, db_name="users.db", query=None, params=()):
+        self.db_name = db_name
+        self.query = query
+        self.params = params
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self
+
+    def execute(self):
+        self.cursor.execute(self.query, self.params)
+        return self.cursor.fetchall()
