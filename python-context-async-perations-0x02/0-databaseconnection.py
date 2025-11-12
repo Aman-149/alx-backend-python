5190e035def6041b56bdf765e0afabf803a007ca
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-12 22:27:25 +0300

    Update 0-databaseconnection.py

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,1 +1,22 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name="test.db"):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        # Open the database connection
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Commit if no exception, else rollback
+        if exc_type is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
+        # Close the connection
+        self.conn.close()
 
