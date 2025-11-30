efd94c3fc8d615eb23a06c9e753a76d79467550a
Aman-149 <amanuelfikadu23971@gmail.com>
2025-11-30 21:15:42 +0300

    Clarify import of signals in MessagingConfig
    Added a comment to clarify the import of signals.

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
 
+
 class MessagingConfig(AppConfig):
     name = 'messaging'
 
     def ready(self):
-        import messaging.signals
-
+        # Import signals to register receivers
+        import messaging.signals  # noqa: F401
